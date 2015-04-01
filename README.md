5cdc6b4ace6a52543d200ad0e3096fb5e1ba25f1
EC2 Default User <ec2-user@ip-172-31-1-182.ec2.internal>
2015-04-01 19:29:14 +0000

    Massive changes + ReadSampler

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # simGene
 A project for a Computational Biology class; Simulates DNA sequencing reads and detects Copy Number Variations
+
+1. Confirm Ref and Alternate sequences are different
+2. Create two error-free sequences from ref and alternate with 25bp (subject to change)
+3. Create random sampler that works in fetal fraction distribution
--- ReadGenerator.py
+++ ReadGenerator.py
@@ -1,25 +1,37 @@
+#!/usr/bin/python
 '''
 Created on Mar 27, 2015
 '''
 
 from subprocess import call
 from os.path import isfile
+from multiprocessing import Process
 import os
-from datetime import datetime
+from datetime import datetime as dt
+from time import sleep
 
-def removeFiles():
-    if isfile("data/output_dat.aln"):
-        os.remove("data/output_dat.aln")
-    if isfile("data/output_dat.fq"):
-        os.remove("data/output_dat.fq")
-        
-def extractReads():
+#Constants
+MAXNUMPROCS = 10
+path = {}
+path['REF'] = 'REF.fa'
+path['ALT'] = 'ALT.fa'
+
+def removeFiles(target):
+    if isfile("data/%s.aln" % target):
+        os.remove("data/%s.aln" % target)
+    if isfile("data/%s.fq" % target):
+        os.remove("data/%s.fq" % target)
+    if isfile("data/%s.sam" % target):
+        os.remove("data/%s.sam" % target)
+
+def extractReads(target):
     print "Extracting data..."
-    if not isfile("data/output_dat.sam"):
+    if not isfile("data/" + target + "_errFree.sam"):
+        print("Extraction target not found: %s" % target)
         return False
-    filename = datetime.now()
-    with open("data/output_dat.sam") as input_file:
-        with open("data/" + str(filename), "a") as output_file:
+
+    with open("data/" + target + "_errFree.sam") as input_file:
+        with open("data/reads/" + target, "a") as output_file:
             for line in input_file:
                 #skip header files
                 if line[0] == "@":
@@ -30,22 +42,63 @@ def extractReads():
                 position = line_components[3]
                 read = line_components[9]
                 output_file.write(position + "    " + read + "\n")
-    os.remove("data/output_dat.sam")
+    os.remove("data/%s_errFree.sam" % target)
     return True
 
+def main(seq, cov):
+    # Sets filename to Month-Day-Hour-Minute-Seconds name
+    filename = (str(x) for x in dt.now().timetuple()[1:6])
+    filename = reduce(lambda x,y: x + "-" +  y, filename)
+
+    # For some reason can't have multiple reads on same file
+    indcopy = filename + ".cp"
+    print("Copying %s to %s" % (path[seq], indcopy))
+    call("cp data/%s data/%s" % (path[seq], indcopy), shell=True)
+
+    print "Generating reads..."
+    toCall = "art_illumina -ef -sam -i data/%s -l 25 -ss HS25 -f %s -o data/%s" % (indcopy, cov, filename)
+    print(toCall)
+    return_code = call(toCall, shell=True)
+    os.remove("data/%s" % indcopy)
+
+    if return_code != 0:
+        print("Error occurred generating reads for %s" % filename)
+        print(return_code)
+        return
+
+    print "Reads generated successfully"
+    removeFiles(filename)
+    result = extractReads(filename)
+
+    if not result:
+        print "Error occurred extracting the reads"
+        return
+
+    print "Reads have been extracted."
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("s", 
+        metavar="Sequence", 
+        type=str, 
+        choices=["REF","ALT"], 
+        help="REFerence or ALTernate sequence.")
+parser.add_argument("c",
+        metavar="Coverage",
+        type=int,
+        default=1,
+        help="Read coverage over any position.")
+
+parser.add_argument("-n", 
+        metavar="Iterations", 
+        type=int,
+        default=1,
+        help="Number of runs.")
+
 if __name__ == '__main__':
-    i = 0
-    while i < 10:
-	print "Generating reads..."
-        return_code = call("art_illumina -sam -i data/hs_alt_CHM1_1.1_chr22.fa -l 25 -ss HS25 -f 10 -o data/output_dat", shell=True)
-#        if return_code != 0:
-#            print "Error occurred generating the reads"
-#            continue
-        print "Reads generated successfully"
-        removeFiles()
-        result = extractReads()
-        if not result:
-            print "Error occurred extracting the reads"
-            continue
-	print "Reads have been extracted."
-        i += 1
+    args = parser.parse_args()
+    arg = "(%s, %s)" % (args.s, args.c)
+    p = [Process(target=main, args=(args.s,args.c)) for _ in range(args.n)]
+    for i in p:
+        i.start()
+        sleep(1)
