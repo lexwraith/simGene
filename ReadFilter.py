7b676637bf3e65d8119ed3e98a17dee917b7fb52
Kevin Michael Walters <kmw2168@columbia.edu>
2015-05-01 15:57:46 -0400

    Script to filter reads for only chromosome 22

---
+++ ReadFilter.py
@@ -0,0 +1,28 @@
+'''
+Created on May 1, 2015
+
+@author: Kevin Walters
+'''
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("i", 
+        metavar="Input", 
+        type=str,  
+        help="Input file to be filtered")
+parser.add_argument("o",
+        metavar="Output",
+        type=str,
+        default=1,
+        help="Output file")
+
+def main(input_file, output_file):
+    with open(input_file, "r") as input:
+        with open(output_file, "w") as output:
+            for line in input:
+                if "chr22" in line:
+                    output.write(line)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args.i, args.o)
