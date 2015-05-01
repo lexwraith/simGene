d9f225a513266070199ddfdf16b8f7548a5b14ff
Kevin Michael Walters <kmw2168@columbia.edu>
2015-05-01 16:06:39 -0400

    Changed output file format to only include necessary fields

--- ReadFilter.py
+++ ReadFilter.py
@@ -3,6 +3,8 @@ Created on May 1, 2015
 '''
 
 import argparse
+from config import *
+
 parser = argparse.ArgumentParser()
 parser.add_argument("i", 
         metavar="Input", 
@@ -14,12 +16,28 @@ parser.add_argument("o",
         default=1,
         help="Output file")
 
+def getLoc(pos):
+    pos = int(pos)
+    for elem in LOC:
+        if elem[0] < pos < elem[1]:
+            return elem[2]
+
 def main(input_file, output_file):
     with open(input_file, "r") as input:
         with open(output_file, "w") as output:
             for line in input:
-                if "chr22" in line:
-                    output.write(line)
+                #skip header files
+                if line[0] == "@":
+                    continue
+                line_components = line.split()
+                if len(line_components) < 4:
+                    continue
+                chromosome = line_components[2]
+                if chromosome != "chr22":
+                    continue
+                position = line_components[3]
+                read = line_components[9]
+                output_file.write("%s,%s,%s\n" % (position,read,getLoc(position)))
 
 if __name__ == '__main__':
     args = parser.parse_args()
